30b7ff4d7db1c2b0dd95eac7b8f1363da55f358d
tommy <tommy8234@gmail.com>
2022-02-18 20:05:43 +0800

    Finish most of the functions in queue.c
    Functions q_reverse and q_sort wait to be finished.

--- queue.c
+++ queue.c
@@ -17,11 +17,28 @@
  */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *queue = malloc(sizeof(struct list_head));
+    if (!queue)
+        return NULL;
+    INIT_LIST_HEAD(queue);
+    return queue;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l)
+        return;
+    struct list_head *tmp = l->next;
+    while (!list_empty(l)) {
+        list_del(tmp);
+        element_t *ele = container_of(tmp, element_t, list);
+        free(ele->value);
+        free(ele);
+        tmp = l->next;
+    }
+    free(l);
+}
 
 /*
  * Attempt to insert element at head of queue.
@@ -32,6 +49,18 @@ void q_free(struct list_head *l) {}
  */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *ele = malloc(sizeof(element_t));
+    if (!ele)
+        return false;
+    ele->value = malloc(strlen(s) + 1);
+    if (!ele->value) {
+        free(ele);
+        return false;
+    }
+    strncpy(ele->value, s, strlen(s) + 1);
+    list_add(&ele->list, head);
     return true;
 }
 
@@ -44,6 +73,18 @@ bool q_insert_head(struct list_head *head, char *s)
  */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *ele = malloc(sizeof(element_t));
+    if (!ele)
+        return false;
+    ele->value = malloc(strlen(s) + 1);
+    if (!ele->value) {
+        free(ele);
+        return false;
+    }
+    strncpy(ele->value, s, strlen(s) + 1);
+    list_add_tail(&ele->list, head);
     return true;
 }
 
@@ -63,7 +104,16 @@ bool q_insert_tail(struct list_head *head, char *s)
  */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    struct list_head *target = head->next;
+    element_t *tmp = container_of(target, element_t, list);
+    list_del_init(target);
+    if (sp) {
+        strncpy(sp, tmp->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return tmp;
 }
 
 /*
@@ -72,7 +122,16 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
  */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    struct list_head *target = head->prev;
+    element_t *tmp = container_of(target, element_t, list);
+    list_del_init(target);
+    if (sp) {
+        strncpy(sp, tmp->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return tmp;
 }
 
 /*
@@ -91,10 +150,17 @@ void q_release_element(element_t *e)
  */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *node;
+    int count = 0;
+    list_for_each (node, head)
+        count++;
+    return count;
 }
 
 /*
+ * https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
  * Delete the middle node in list.
  * The middle node of a linked list of size n is the
  * ⌊n / 2⌋th node from the start using 0-based indexing.
@@ -104,11 +170,28 @@ int q_size(struct list_head *head)
  */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return NULL;
+    struct list_head *node;
+    int count = 0, mid = 0;
+    list_for_each (node, head)
+        count++;
+    mid = (float) count / 2 > count / 2 ? count / 2 + 1 : count / 2;
+    count = 0;
+    element_t *ele;
+    list_for_each_entry (ele, head, list) {
+        if (++count == mid) {
+            list_del(&ele->list);
+            free(ele->value);
+            free(ele);
+            break;
+        }
+    }
     return true;
 }
 
 /*
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
  * Delete all nodes that have duplicate string,
  * leaving only distinct strings from the original list.
  * Return true if successful.
@@ -119,16 +202,56 @@ bool q_delete_mid(struct list_head *head)
  */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+    if (list_empty(head) || list_is_singular(head))
+        return true;
+    struct list_head *start = head->next, *end = start->next;
+    while (start != head) {
+        element_t *ele1 = container_of(start, element_t, list);
+        element_t *ele2 = container_of(end, element_t, list);
+        bool found = false;
+        while (strcmp(ele1->value, ele2->value) == 0) {
+            found = true;
+            end = end->next;
+            if (end == head)
+                break;
+            ele2 = container_of(end, element_t, list);
+        }
+        while (start != end) {
+            struct list_head *next = start->next;
+            if (found) {
+                list_del(start);
+                element_t *tmp = container_of(start, element_t, list);
+                free(tmp->value);
+                free(tmp);
+            }
+            start = next;
+        }
+        end = start->next;
+    }
     return true;
 }
 
 /*
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
  * Attempt to swap every two adjacent nodes.
  */
 void q_swap(struct list_head *head)
 {
-    // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *first = head->next, *second = first->next;
+    element_t *ele1, *ele2;
+    while (first != head && second != head) {
+        ele1 = container_of(first, element_t, list);
+        ele2 = container_of(second, element_t, list);
+        char *tmp = ele1->value;
+        ele1->value = ele2->value;
+        ele2->value = tmp;
+        first = first->next->next;
+        second = second->next->next;
+    }
 }
 
 /*
