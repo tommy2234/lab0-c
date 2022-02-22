8864010e2b4f584c8014ebc43d3f10f8ced75ed4
tommy <tommy8234@gmail.com>
2022-02-22 09:57:31 +0800

    Add list_sort.c and list_sort.h
    Seperate codes in lib/list_sort.c from qtest.c and
    make list_sort.c an independent source file.

---
+++ list_sort.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_LIST_SORT_H
+#define _LINUX_LIST_SORT_H
+
+#include "list.h"
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+typedef unsigned char u8;
+
+/*
+ * __attribute__((nonnull(2,3)))
+ * The above macro means that 2nd and 3rd arg must be Non-NULL
+ */
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp);
+#endif
